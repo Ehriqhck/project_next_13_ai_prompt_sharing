3219a50e08c87d28484311b1d152d868a6ad453e
Eric <eyang701@gmail.com>
2024-10-11 18:28:42 -0400

    landingicon

---
+++ components/generic/Icons/LandingIcon.jsx
@@ -0,0 +1,14 @@
+import * as React from "react"
+const LandingIcon = (props) => (
+  <svg width={props.width} height={props.height} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path
+      fill="#98FDDF"
+      d="M18.601 9.522 6.997 7.485 4.25 2.482l-2.203-.387-.042 4.514-1.617-.284-.155.878a9.153 9.153 0 0 0 5.921 3.752l1.544.27-2.032 3.26 2.644.465 6.439-2.488 3.231.567c1.621.284 3.076-.271 3.248-1.238.171-.968-1.006-1.985-2.627-2.27Z"
+    />
+    <path
+      fill="#01CFAD"
+      d="M20.586 18.124H.89a.89.89 0 1 0 0 1.78h19.696a.89.89 0 0 0 0-1.78Z"
+    />
+  </svg>
+)
+export default LandingIcon
